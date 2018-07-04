34badca2c29eb3456a382efe505d71c9be4b8d06
lwchina <liuweichinash@163.com>
2018-07-04 18:10:17 +0800

    new file:   eureka-server/pom.xml 	new file:   eureka-server/src/main/java/cloud/eureka/server/EurekaServerApplication.java 	new file:   eureka-server/src/main/resources/application.properties 	new file:   pom.xml

---
+++ eureka-server/src/main/java/cloud/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package cloud.eureka.server;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author wei.liu
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2018-07-04
+ * Time: 17:40
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        StringUtils.isNotBlank("");
+    }
+}
